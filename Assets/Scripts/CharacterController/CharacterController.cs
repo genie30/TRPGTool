d4863ed730aa14804168d33da4d2637a3a425067
genie30 <geniegamesandtools@gmail.com>
2020-01-24 19:30:58 +0900

    駒ドラッグするところまでやった。

--- Assets/Scripts/CharacterController/CharacterController.cs
+++ Assets/Scripts/CharacterController/CharacterController.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
 
-public class CharacterController : MonoBehaviour
+public class CharacterController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    // GMとかでドラッグできる判定をつける
+
+    Tilemap tilemap;
+    bool drag;
+    int minx = -13, maxx = 11, miny = -11, maxy = 10;
+
+    Vector3 pos;
+
+    private void Start()
     {
-        
+        tilemap = FindObjectOfType<Tilemap>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(drag) GridMove(pos);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        drag = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        drag = false;
+    }
+
+    private void GridMove(Vector3 pos)
+    {
+        var wpos = tilemap.WorldToCell(pos);
+        transform.position = wpos;
+        Debug.Log("move" + wpos);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        pos = Camera.main.ScreenToWorldPoint(eventData.position);
+        pos.z = 10f;
     }
 }
