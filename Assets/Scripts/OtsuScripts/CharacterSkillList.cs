0eb96a9714e4b9df880101b023efa5b4ce2e7189
OtsuKent <plutokent1@gmail.com>
2020-01-25 23:14:05 +0900

    スキル作成UI(プレファブ)とscript追加

---
+++ Assets/Scripts/OtsuScripts/CharacterSkillList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSkillList : MonoBehaviour
+{
+    public static CharacterSkillList instance;
+    public List<CharacterSkill> skillList = new List<CharacterSkill>();
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public CharacterSkill SearchName(string name)
+    {
+        foreach (CharacterSkill skill in skillList)
+        {
+            if (skill.name.Contains(name))
+            {
+                return skill;
+            }
+        }
+        return null;
+    }
+}
